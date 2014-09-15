f8816c5339824265e0f6434d4d5919689018ec3d
Simon <s.shiharev@topface.com>
2014-09-15 19:34:06 +0400

    тыцтцы

---
+++ roudmap.php
@@ -0,0 +1,16 @@
+<?php
+
+$url = "https://tasks.verumnets.ru/issues.json";
+
+$url .= "?" . http_build_query(array(
+    "project_id" => 29,
+    "key" =>"553a52a60d91dd82cfc74f056f509b747c336c7c"
+));
+
+
+
+
+echo json_decode(file_get_contents($url));
+
+
+?>
